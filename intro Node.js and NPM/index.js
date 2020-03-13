218b1c6396de72134b434564213ab89b80242bac
Poperechnyi Stepan <spoperechnyi@exadel.net>
2020-03-13 18:19:13 +0200

    parsing variables from url

--- intro Node.js and NPM/index.js
+++ intro Node.js and NPM/index.js
@@ -36,10 +36,11 @@ const tempProduct = fs.readFileSync(
 );
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  console.log(pathname);
 
   // Overview page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHTML = dataObject
@@ -50,12 +51,15 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     // Product page
-  } else if (pathName === "/product") {
+  } else if (pathname === "/product") {
     res.writeHead(200, { "Content-type": "text/html" });
-    res.end(tempProduct);
+    const product = dataObject[query.id];
+    console.log(product);
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     //API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
 
