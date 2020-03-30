75e79c5e6373c42255b87939ad4fa14104313f37
Poperechnyi Stepan <spoperechnyi@exadel.net>
2020-03-30 18:18:06 +0300

    add env

--- NATOURS/app.js
+++ NATOURS/app.js
@@ -6,7 +6,10 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRouter');
 
 // 1 MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
