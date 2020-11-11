859e8ea18954008e9061db58d4f43e534f140540
Stepan Poperechnyi <poperechnyi1@gmail.com>
2020-11-11 07:40:11 +0200

    error handling

--- NATOURS/app.js
+++ NATOURS/app.js
@@ -13,10 +13,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -26,5 +22,30 @@ app.use((req, res, next) => {
 // 2) ROUTS
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next)=>{
+  // res.status(404).json({
+  //   status:'fail',
+  //   message:`Can't find ${req.originalUrl} on this server!`
+  // })
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`)
+  err.status = 'fail'
+  err.statusCode = 404
+
+  next(err)
+})
+
+app.use((err, req, res, next)=>{
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || "error"
+
+  res.status(err.statusCode).json(
+    {
+      status: err.statusCode,
+      message: err.message
+    }
+  )
+})
 
 module.exports = app;
