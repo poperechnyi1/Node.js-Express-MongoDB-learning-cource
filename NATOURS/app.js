79efeaacd9940b6b4dad292f95f5e2ff669ee460
Stepan Poperechnyi <poperechnyi1@gmail.com>
2020-11-12 22:18:01 +0200

    better error handling and refactoring

--- NATOURS/app.js
+++ NATOURS/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRouter');
 
@@ -24,28 +26,10 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next)=>{
-  // res.status(404).json({
-  //   status:'fail',
-  //   message:`Can't find ${req.originalUrl} on this server!`
-  // })
 
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`)
-  err.status = 'fail'
-  err.statusCode = 404
-
-  next(err)
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
 })
 
-app.use((err, req, res, next)=>{
-  err.statusCode = err.statusCode || 500
-  err.status = err.status || "error"
-
-  res.status(err.statusCode).json(
-    {
-      status: err.statusCode,
-      message: err.message
-    }
-  )
-})
+app.use(globalErrorHandler)
 
 module.exports = app;
