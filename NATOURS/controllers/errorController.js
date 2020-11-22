b595a361897181b2c9fd27e966f29a5b8c0437d0
Stepan Poperechnyi <poperechnyi1@gmail.com>
2020-11-22 12:21:40 +0200

    duplicated database field

--- NATOURS/controllers/errorController.js
+++ NATOURS/controllers/errorController.js
@@ -1,3 +1,18 @@
+const AppError = require('./../utils/appError')
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`
+  return new AppError(message, 404)
+}
+
+const handleDupricatredFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0]
+  console.log(10, value)
+  const message = `Duplicate field value ${value}. Please use another value!`
+
+  return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json(
     {
@@ -41,6 +56,10 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === "development") {
       sendErrorDev(err, res)
     } else if (process.env.NODE_ENV === "production") {
-      sendErrorProduction(err, res)
+      let error = {...err}
+      if (error.name === "CastError") error = handleCastErrorDB(error)
+      if (error.code === 11000) error = handleDupricatredFieldsDB(error)
+
+      sendErrorProduction(error, res)
     }
 }
