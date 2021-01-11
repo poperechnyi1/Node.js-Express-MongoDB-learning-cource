81ca72b3899af3316a4041a7c4898de52465f2a5
Stepan Poperechnyi <poperechnyi1@gmail.com>
2021-01-11 21:00:52 +0200

    add protection to tour routes

--- NATOURS/routes/tourRoutes.js
+++ NATOURS/routes/tourRoutes.js
@@ -10,6 +10,7 @@ const {
   getTourStats,
   getMonthlyPlan,
 } = require('./../controllers/tourController');
+const authController = require('./../controllers/authController')
 
 // router.param('id', checkID);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
@@ -17,7 +18,7 @@ router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan)
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(authController.protect, getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
