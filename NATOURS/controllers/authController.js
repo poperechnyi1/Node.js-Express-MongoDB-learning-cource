87e3e9d5c759b91e6100e84fa32f1d6c2d5805ca
Stepan Poperechnyi <poperechnyi1@gmail.com>
2021-01-27 22:42:49 +0200

    add role to the object

--- NATOURS/controllers/authController.js
+++ NATOURS/controllers/authController.js
@@ -16,7 +16,8 @@ const newUser = await User.create({
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangedAt
+    passwordChangedAt: req.body.passwordChangedAt,
+    role: req.body.role
 })
 
 const token = signToken(newUser._id)
@@ -86,3 +87,16 @@ exports.protect = catchAsync(async (req, res, next)=> {
     req.user = currentUser;
     next()
 })
+
+exports.restrictTo = (...roles)=> {
+    return (req, res, next) => {
+        // roles ['admin', 'lead-guide'] . role = 'user'
+        console.log(req.user.role)
+        console.log(req.user)
+        if (!roles.includes(req.user.role)) {
+            return next(new AppError('You do not have permission to perform this action', 403)) 
+        }
+
+        next();
+    }
+}
