50b07d82816629e15b382ab7aeab062c66d6d9a2
Stepan Poperechnyi <poperechnyi1@gmail.com>
2021-01-31 17:46:09 +0200

    use patch instead of post

--- NATOURS/routes/userRouter.js
+++ NATOURS/routes/userRouter.js
@@ -20,7 +20,7 @@ router.route('/signup').post(signup)
 router.route('/login').post(login)
 
 router.route('/forgotPassword').post(forgotPassword)
-router.route('/resetPassword').post(resetPassword)
+router.route('/resetPassword/:token').patch(resetPassword)
 
 router
   .route('/')
