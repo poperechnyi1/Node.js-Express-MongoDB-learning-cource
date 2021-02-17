01a95299ea7a389e0225db8f190511fce4eba172
Stepan Poperechnyi <poperechnyi1@gmail.com>
2021-02-17 22:24:53 +0200

    use rate limit package

--- NATOURS/app.js
+++ NATOURS/app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const rateLimit = require('express-rate-limit')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRouter');
 
-// 1 MIDDLEWARES
+// 1 GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 3,
+  windowM: 69 * 60 * 10000,
+  message: 'Too many requests from this IP, please try again in an hour!' 
+})
+
+app.use('/api', limiter)
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
