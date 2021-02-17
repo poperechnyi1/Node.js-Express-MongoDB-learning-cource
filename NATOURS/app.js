4c79666025e5714027ae068f84a02d16f7ca2175
Stepan Poperechnyi <poperechnyi1@gmail.com>
2021-02-17 22:55:37 +0200

    use helmet

--- NATOURS/app.js
+++ NATOURS/app.js
@@ -7,12 +7,18 @@ const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRouter');
+const helmet = require('helmet')
 
 // 1 GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet())
+
+// Development loggin
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 3,
   windowM: 69 * 60 * 10000,
@@ -21,10 +27,14 @@ const limiter = rateLimit({
 
 app.use('/api', limiter)
 
-app.use(express.json());
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
   next();
