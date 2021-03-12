0e8f2ef347a961967a11498597f8a715ef7f0458
Stepan Poperechnyi <poperechnyi1@gmail.com>
2021-03-12 22:22:54 +0200

    use factory for deleting

--- NATOURS/app.js
+++ NATOURS/app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const rateLimit = require('express-rate-limit')
-const AppError = require('./utils/appError')
-const globalErrorHandler = require('./controllers/errorController')
+const rateLimit = require('express-rate-limit');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRouter');
-const reviewRouter = require('./routes/reviewRoutes')
-const helmet = require('helmet')
-const mongooseSanitize = require('express-mongo-sanitize')
-const xss = require('xss-clean')
-const hpp = require('hpp')
+const reviewRouter = require('./routes/reviewRoutes');
+const helmet = require('helmet');
+const mongooseSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 // 1 GLOBAL MIDDLEWARES
 // Set security HTTP headers
-app.use(helmet())
+app.use(helmet());
 
 // Development loggin
 if (process.env.NODE_ENV === 'development') {
@@ -23,54 +23,54 @@ if (process.env.NODE_ENV === 'development') {
 
 // Limit requests from same API
 const limiter = rateLimit({
-  max: 3,
+  max: 50,
   windowM: 69 * 60 * 10000,
-  message: 'Too many requests from this IP, please try again in an hour!' 
-})
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
 
-app.use('/api', limiter)
-
+app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
 //DATA santization against NoSQL, query injections
-app.use(mongooseSanitize())
+app.use(mongooseSanitize());
 
 // Data sanitization against XSS
-app.use(xss())
+app.use(xss());
 
 //Prevent parameter pollution
-app.use(hpp({
-  whitelist: [
-    'duration', 
-    'ratingsQuantity', 
-    'ratingsAverage',
-    'maxGroupSize', 
-    'difficulty', 
-    'price'
-  ]
-}))
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString()
+  req.requestTime = new Date().toISOString();
   next();
 });
 
 // 2) ROUTS
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/reviews', reviewRouter)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
-app.all('*', (req, res, next)=>{
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
-})
-
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 module.exports = app;
--- NATOURS/controllers/userController.js
+++ NATOURS/controllers/userController.js
@@ -1,90 +1,86 @@
-const AppError = require('../utils/appError')
-const User = require('./../models/userModel')
-const catchAsync = require('./../utils/catchAsync')
+const AppError = require('../utils/appError');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 const filterObj = (obj, ...allowedFields) => {
-  const newObj = {}
-  Object.keys(obj).forEach(el => {
-    if(allowedFields.includes(el)) {
-      newObj[el] = obj[el]
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
     }
-  })
-  return newObj
-}
+  });
+  return newObj;
+};
 
-exports.getAllUsers = catchAsync( async (req, res, next) => {
-  const users = await User.find()
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
 
   res.status(200).json({
     status: 'success',
     result: users.length,
     data: {
-      users
-    }
+      users,
+    },
   });
 });
 
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet define'
+    message: 'This route is not yet define',
   });
 };
 
-exports.updateMe = catchAsync( async (req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) create error if user POSTs password data
-  if(req.body.password || req.body.passwordConfirm) {
+  if (req.body.password || req.body.passwordConfirm) {
     return next(
       new AppError(
-        'This route is not for password updates. Please use /updateMyPassword.', 
+        'This route is not for password updates. Please use /updateMyPassword.',
         400
       )
-    )
+    );
   }
 
   // 2) Filtered out unwanted fields names that are not allowed to be updated
-  const filteredBody = filterObj(req.body, 'name', 'email')
+  const filteredBody = filterObj(req.body, 'name', 'email');
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-      new: true, 
-      runValidators: true
-  })
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({
     status: 'success',
     data: {
-      user: updatedUser
-    }
-  })
-})
+      user: updatedUser,
+    },
+  });
+});
 
-exports.deleteMe = catchAsync(async (req, res, next)=>{
-  await User.findByIdAndUpdate(req.user.id, {active: false})
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
 
   res.status(204).json({
     status: 'success',
-    date: null
-  })
-})
+    date: null,
+  });
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet define'
+    message: 'This route is not yet define',
   });
 };
 
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet define'
+    message: 'This route is not yet define',
   });
 };
 
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet define'
-  });
-};
+exports.deleteUser = factory.deleteOne(User);
