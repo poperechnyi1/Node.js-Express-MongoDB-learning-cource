df482457ae124ae386a1fff1d3de20bee588ca64
Stepan Poperechnyi <poperechnyi1@gmail.com>
2021-03-17 22:28:59 +0200

    calculating average rating on Tours

--- NATOURS/routes/reviewRoutes.js
+++ NATOURS/routes/reviewRoutes.js
@@ -19,11 +19,11 @@ router
   .route('/:id')
   .get(reviewController.getReview)
   .patch(
-    authController.restrictTo('users', 'admin'),
+    authController.restrictTo('user', 'admin'),
     reviewController.updateReview
   )
   .delete(
-    authController.restrictTo('users', 'admin'),
+    authController.restrictTo('user', 'admin'),
     reviewController.deleteReview
   );
 
