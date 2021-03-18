dbb27b01adb5093e1f1770f38a965791176aaabe
Stepan Poperechnyi <poperechnyi1@gmail.com>
2021-03-18 10:13:51 +0200

    prevent duplicate reviews

--- NATOURS/models/tourModel.js
+++ NATOURS/models/tourModel.js
@@ -36,6 +36,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must above 1.0'],
       max: [5, 'Rating must below 5.0'],
+      set: (val) => Math.round(val * 10), // 4.666666,, 46.666 47 4.7
     },
     ratingsQuantity: {
       type: Number,
